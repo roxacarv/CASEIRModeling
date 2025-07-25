9c548a20788429b314b55d9b5d8fabed13c8bee7
Rodrigo Xavier <xavier.rc791@gmail.com>
2025-07-25 16:59:20 -0300

    enhance: add new functions to move and update cells add basic model description that will be used in the project

--- seir.c
+++ seir.c
@@ -1,5 +1,8 @@
 #include "seir.h"
 
+// Create a SEIR model on the grid, by defining the initial states of the cells
+// It randomly assigns states such as (I) and (E) to some cells
+// Other cells will remain susceptible (S)
 void create_seir_model(Grid *grid) {
     srand(time(NULL)); // Seed the random number generator
     if (!grid) {
@@ -30,4 +33,38 @@ void create_seir_model(Grid *grid) {
     printf("Grid dimensions: %d x %d\n", grid->width, grid->height);
     printf("Infectious cells: %d, Exposed cells: %d\n", infectious_cells, exposed_cells);
     print_grid(grid);
+}
+
+// Assign a different state to a given cell
+// Should be called when a cell suffer a state change based on probabilities
+void assign_state(Cell *cell, CellState state) {
+    if (cell) {
+        cell->value = state;
+    } else {
+        fprintf(stderr, "Cell is NULL\n");
+    }
+}
+
+void get_cell_state(const Cell *cell, CellState *state) {
+    if (cell && state) {
+        *state = cell->value;
+    } else {
+        fprintf(stderr, "Cell or state pointer is NULL\n");
+    }
+}
+
+// Moves a cell through the grid, to simulate its movement as if it were a person
+void move_cell(Grid *grid, Cell *cell, int new_x, int new_y) {
+    if (!grid || !cell) {
+        fprintf(stderr, "Grid or cell is NULL\n");
+        return;
+    }
+    
+    if (new_x < 0 || new_x >= grid->width || new_y < 0 || new_y >= grid->height) {
+        fprintf(stderr, "New position out of bounds\n");
+        return;
+    }
+
+    cell->x = new_x;
+    cell->y = new_y;
 }
