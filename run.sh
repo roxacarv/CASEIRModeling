993dc39ebffd18770946a093c4a1c6ea123b2bc0
Rodrigo Xavier <xavier.rc791@gmail.com>
2025-08-04 15:48:52 -0300

    fix: check for python venv before trying to use it

--- run.sh
+++ run.sh
@@ -21,7 +21,11 @@ check_python_libraries() {
 if [ ${exit_status} -eq 0 ]; then
     echo "Simulation has finished successfully. Calling Python to generate plot analyzes"
     cd "csv"
-    . venv/bin/activate
+    if ! -d "venv"; then
+        python3 -m venv "${PYTHON_DIR}"
+        echo "Python venv was created at ${PYTHON_DIR}"
+    fi
+    source "venv/bin/activate"
     check_python_libraries # Check if Python libraries are installed before proceeding
     python3 analysis_plt.py
     echo "Python has finished analyzing the data."
